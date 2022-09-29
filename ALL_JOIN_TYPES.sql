ba903b728b4ddea3553c7f88cafcb046860cfd62
mehmetseymusyuzen <mehmtt.21x@gmail.com>
2022-09-29 12:26:00 +0200

    LEFT JOIN and RIGHT JOIN and FULL JOIN

---
+++ ALL_JOIN_TYPES.sql
@@ -0,0 +1,8 @@
+SELECT city.city, country.country FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+
+SELECT  customer.first_name, customer.last_name, payment.payment_id FROM customer
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
+
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
